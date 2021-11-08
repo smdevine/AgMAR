6dad7d4a378be666abae36f88239d7e5342df220
smdevine <smdevine@ucdavis.edu>
2021-11-08 13:54:34 -0800

    some changes not previously committed for lab experiment simulations

--- RZWQM/columnLeachingdata.R
+++ RZWQM/columnLeachingdata.R
@@ -10,7 +10,7 @@ workDir <- 'C:/Users/smdevine/Desktop/post doc/Dahlke/RZWQM/projects/ColumnNO3Le
 projectName <- 'FineSandyLoam_v11' #was last using v8
 list.files(file.path(workDir, projectName))
 
-dailyPT <- read.table(file.path(workDir, projectName, 'DAILY.PLT'), col.names = c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'H2O BRK THROUGH CURVE (CM/DAY)', 'TEMP BREAK THROUGH CURVE (C)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL RUNOFF (CM)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)', 'N2O PRODUCTION (KG N/HA)', 'NxO PRODUCTIOIN (KG N/HA)'), header = FALSE, skip=100)
+dailyPT <- read.table(file.path(workDir, projectName, 'DAILY.PLT'), col.names = c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), header = FALSE, skip=65)
 dailyPT$DAY
 dailyPT$TOTAL.NO3.N.IN.PROFILE..KG.HA.
 sum(dailyPT$NO3.FLUX.INTO.GW..UG.CM.2.DAY.)/10
@@ -49,7 +49,10 @@ dev.off()
 #function to graph
 #for v8: c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'H2O BRK THROUGH CURVE (CM/DAY)', 'TEMP BREAK THROUGH CURVE (C)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL RUNOFF (CM)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)', 'N2O PRODUCTION (KG N/HA)', 'NxO PRODUCTIOIN (KG N/HA)')
 
-plotActVsSim <- function(subDir, plot_fname, ylim_upp, columnNames, skiplines) {
+plotActVsSim <- function(subDir, plot_fname, ylim_upp, columnNames, skiplines, ResidualNO3, fig_label, makeLegend=FALSE) {
+  nutri <- read.table(file.path(workDir, subDir, 'nutri.PLT'), col.names = 1:19, header = FALSE, skip=104)
+  denitrified <- sum(nutri[12])
+  print(paste(denitrified, 'kg ha-1 denitrification'))
   dailyPT <- read.table(file.path(workDir, subDir, 'DAILY.PLT'), col.names = columnNames, header = FALSE, skip=skiplines)
   print(paste(dailyPT$TOTAL.NO3.N.IN.PROFILE..KG.HA.[nrow(dailyPT)], 'kg ha-1 residual nitrate in profile'))
   print(paste(sum(dailyPT$NO3.FLUX.INTO.GW..UG.CM.2.DAY.[1:27]/10), 'kg ha-1 total nitrate leached'))
@@ -57,11 +60,15 @@ plotActVsSim <- function(subDir, plot_fname, ylim_upp, columnNames, skiplines) {
   leachingDF <- read.table(file.path(workDir, subDir, 'CLEACH.OUT'), col.names = c('DAY', 'H', 'CA', 'NA', 'MG', 'CL', 'HCO3', 'SO4', 'AL', 'NO3-N', 'NH4-N', 'CO3', 'UREA-N', 'PEST #1', 'PEST #2', 'PEST #3'), header = FALSE, skip=6)
   tiff(file = file.path(FiguresDir, plot_fname), family = 'Times New Roman', width = 4.5, height = 3.25, pointsize = 12, units = 'in', res=800, compression = 'lzw')
   par(mar=c(3,3.5,0.25,0.25))
-  plot(actualNO3$time_daily, actualNO3$gNO3_cm2*10^5, ylim = c(0,ylim_upp), ylab='', xlab='', xlim = c(0,34))
-  mtext(expression('NO'[3]*'-N leached (kg ha'^-1*')'), side = 2, line=2.25)
+  plot(actualNO3$time_daily, actualNO3$gNO3_cm2*10^5, ylim = c(0,ylim_upp), ylab='', xlab='', xlim = c(0,36))
+  mtext(expression('NO'[3]*'-N (kg ha'^-1*')'), side = 2, line=2.25)
   mtext('days', side=1, line=2)
   points(x=leachingDF$DAY, y=leachingDF$NO3.N/10, col='red')
-  legend(x='topleft', legend=c('measured', 'RZWQM simulated'), pch=1, col=c('black', 'red'), bty='n')
+  lines(x=c(34.5,34.5,35.5,35.5), y=c(0,rep(21.73, 2), 0), type='s', col='black', lty=1)
+  lines(x=c(34.7,34.7,35.3,35.3), y=c(0,rep(ResidualNO3, 2), 0), type='s', col='red', lty=2)
+  lines(x=c(36,36,36.8,36.8), y=c(0,rep(denitrified, 2), 0), type='s', col='red', lty=3)
+  if(makeLegend) {legend(x='topleft', legend=c(expression('observed NO'[3]*' leached'), expression('simulated NO'[3]*' leached'), expression('observed residual NO'[3]), expression('simulated residual NO'[3]), 'simulated denitrification'), pch=c(1, 1, NA, NA, NA), lty=c(NA, NA, 1, 2, 3), col=c('black', 'red', 'black', 'red', 'red'), bty='n', cex=0.85)}
+  text(x=32, y=21, fig_label)
   dev.off()
 }
 
@@ -75,7 +82,7 @@ plotActVsSim('FineSandyLoam_v15', 'cumulative_NO3_actual_vs_RZWQM_v15.tif', 20, 
 plotActVsSim('FineSandyLoam_v16', 'cumulative_NO3_actual_vs_RZWQM_v16.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
 plotActVsSim('FineSandyLoam_v17', 'cumulative_NO3_actual_vs_RZWQM_v17.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
 plotActVsSim('FineSandyLoam_v18', 'cumulative_NO3_actual_vs_RZWQM_v18.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
-plotActVsSim('FineSandyLoam_v19', 'cumulative_NO3_actual_vs_RZWQM_v19.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
+plotActVsSim('FineSandyLoam_v19', 'cumulative_NO3_actual_vs_RZWQM_v19.tif', 22, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65, ResidualNO3 = 21.8, fig_label = 'b')
 plotActVsSim('FineSandyLoam_v20', 'cumulative_NO3_actual_vs_RZWQM_v20.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
 plotActVsSim('FineSandyLoam_v21', 'cumulative_NO3_actual_vs_RZWQM_v21.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
 plotActVsSim('FineSandyLoam_v22', 'cumulative_NO3_actual_vs_RZWQM_v22.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
@@ -84,6 +91,6 @@ plotActVsSim('FineSandyLoam_v24', 'cumulative_NO3_actual_vs_RZWQM_v24.tif', 20, 
 plotActVsSim('FineSandyLoam_v25', 'cumulative_NO3_actual_vs_RZWQM_v25.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
 plotActVsSim('FineSandyLoam_v26', 'cumulative_NO3_actual_vs_RZWQM_v26.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
 plotActVsSim('FineSandyLoam_v27', 'cumulative_NO3_actual_vs_RZWQM_v27.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
-plotActVsSim('FineSandyLoam_v28', 'cumulative_NO3_actual_vs_RZWQM_v28.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
+plotActVsSim('FineSandyLoam_v28', 'cumulative_NO3_actual_vs_RZWQM_v28.tif', 22, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65, ResidualNO3 = 20.54, fig_label = 'c', makeLegend = FALSE)
 plotActVsSim('FineSandyLoam_v29', 'cumulative_NO3_actual_vs_RZWQM_v29.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
-plotActVsSim('FineSandyLoam_v30', 'cumulative_NO3_actual_vs_RZWQM_v30.tif', 20, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65)
+plotActVsSim('FineSandyLoam_v30', 'cumulative_NO3_actual_vs_RZWQM_v30.tif', 22, c('DAY', 'ACCUMULATED INFILTRATION (CM)', 'WATER FLUX INTO GW (CM/DAY)', 'TOTAL NO3-N IN PROFILE (KG/HA)', 'NO3 FLUX INTO GW (UG/CM^2/DAY)', 'NO3 BREAK THROUGH (MG/L)', 'MINERALIZATION (KG/HA)', 'NITRIFICATION (KG/HA)'), 65, ResidualNO3 = 2.44, fig_label = 'a', makeLegend = TRUE)
