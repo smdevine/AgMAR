5794c0bf09538db738bfd00ee00f2cc58a060104
smdevine <smdevine@ucdavis.edu>
2021-12-21 17:58:20 -0800

    added function to extract overall N and water balance info along with harvest records

--- RZWQM/makeLeachingPlots_v2.R
+++ RZWQM/makeLeachingPlots_v2.R
@@ -7,12 +7,6 @@ workDir <- 'C:/Users/smdevine/Desktop/post doc/Dahlke/RZWQM/projects/PulseSoilCl
 FiguresDir <- 'C:/Users/smdevine/Desktop/post doc/Dahlke/RZWQM/summaries/Figures'
 MBresultsDir <- 'C:/Users/smdevine/Desktop/post doc/Dahlke/RZWQM/summaries'
 
-#for scenario names:
-#v1 is soil properties from textural class look-up tables in RZWQM
-#v2 is soil properties from KSSL and Rosetta-estimated Ks and theta at 0.33 bar and 15 bars
-#v3 is soil properties, including Ks and moisture retention, from SSURGO
-
-
 
 cumulativeFluxes <- function(projectName, soil) {
   scenarioDirs <- list.dirs(file.path(workDir, projectName), recursive = FALSE)
@@ -44,8 +38,9 @@ dailyReport<- function(projectName, soil) {
   leachingDF
 }
 
-dailyReport_df <- dailyReport(projectName = 'SteadyStateRuns', 'CoarseSHR')
-# head(dailyReport_df)
+dailyReport_df <- dailyReport(projectName = 'FloodRuns_v1', 'LoamySHR')
+head(dailyReport_df)
+colnames(dailyReport_df)
 # summary(dailyReport_df$TEMP.BREAK.THROUGH.CURVE..C.)
 # Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
 # 3.062  10.603  16.109  15.470  19.573  28.460
@@ -71,7 +66,7 @@ sum(dailyReport_df$NO3.FLUX.INTO.GW..UG.CM.2.DAY.)/10 #1618.488 kg NO3-N ha-1 to
 #after changing to end date of 10/1/20
 #base run: 1339.119 kg N
 #steady state runs:
-#
+#see notebook for results
 
 sum(dailyReport_df$WATER.FLUX.INTO.GW..CM.DAY.) #329 cm H20
 sum(dailyReport_df$NO3.FLUX.INTO.GW..UG.CM.2.DAY.)/sum(dailyReport_df$WATER.FLUX.INTO.GW..CM.DAY.) #49.17823 ppm NO3 in GW recharge
@@ -100,6 +95,48 @@ as.data.frame(tapply(dailyReport_df$NUTRIENT.STRESS, dailyReport_df$year, functi
 as.Date('1984-07-22', format='%Y-%m-%d')
 dailyReport_df[dailyReport_df$date==as.Date('1984-07-22', format='%Y-%m-%d'),]
 
+
+overall_results <- function(projectName, soil) {
+  input <- readLines(file.path(workDir, projectName, soil, 'RZWQM.OUT'), n=2000)
+  initial_soilC <- input[grepl('INITAL TOTAL SOIL C KG C/HA:', input)]
+  initial_soilC <- as.numeric(unlist(strsplit(initial_soilC, ':'))[2]) #in kg C ha-1
+  final_soilC <- input[grepl('FINAL  TOTAL SOIL C KG C/HA:', input)]
+  final_soilC <- as.numeric(unlist(strsplit(final_soilC, ':'))[2]) #in kg C ha-1
+  initial_soilN <- input[grepl('INITAL TOTAL SOIL N KG N/HA:', input)]
+  initial_soilN <- as.numeric(unlist(strsplit(initial_soilN, ':'))[2]) #in kg N ha-1
+  final_soilN <- input[grepl('FINAL  TOTAL SOIL N KG N/HA:', input)]
+  final_soilN <- as.numeric(unlist(strsplit(final_soilN, ':'))[2]) #in kg N ha-1
+  denitrification <- input[grepl('LOSS TO DENITRIFICATION KG N/HA:', input)]
+  denitrification <- as.numeric(unlist(strsplit(denitrification, ':'))[2])
+  NO3_leaching <- input[grepl('LOSS TO SEEPAGE KG N/HA:', input)]
+  NO3_leaching <- as.numeric(unlist(strsplit(NO3_leaching, ':'))[2])
+  N_min <- input[grepl('TOTAL MINERALIZATION', input)][3]
+  N_min <- as.numeric(unlist(strsplit(N_min, 'MINERALIZATION'))[2])
+  N_imm <- input[grepl('TOTAL IMMOBILIZATION', input)]
+  N_imm <- as.numeric(unlist(strsplit(N_imm, 'IMMOBILIZATION'))[2])
+  fert_app <- input[grepl('TOTAL FERTILIZER APP', input)]
+  fert_app <- as.numeric(unlist(strsplit(fert_app, 'APP'))[2])
+  precip <- input[grepl('PRECIPITATION +[(]+CM+[)]', input)]
+  precip <- as.numeric(unlist(strsplit(precip, '[)]'))[2])
+  irr <- input[grepl('IRRIGATION +[(]+CM+[)]', input)]
+  irr <- as.numeric(unlist(strsplit(irr, '[)]'))[2])
+  runoff <- input[grepl('TOTAL SURFACE RUNOFF +[(]+CM+[)]', input)]
+  runoff <- as.numeric(unlist(strsplit(runoff, '[)]'))[2]) 
+  evap <- input[grepl('EVAPORATION FROM SOIL SURFACE +[(]+CM+[)]', input)]
+  evap <- as.numeric(unlist(strsplit(evap, '[)]'))[2])
+  trans <- input[grepl('PLANT UPTAKE FOR TRANSPIRATION +[(]+CM+[)]', input)]
+  trans <- as.numeric(unlist(strsplit(trans, '[)]'))[2])
+  DP <- input[grepl('DEEP SEEPAGE OUT OF PROFILE +[(]+CM+[)]', input)]
+  DP <- as.numeric(unlist(strsplit(DP, '[)]'))[2])
+  result <- data.frame(initial_totalsoilMgC_ha=initial_soilC/1000, final_totalsoilMgC_ha=final_soilC/1000, initial_totalsoilkgN_ha=initial_soilN, final_totalsoilkgN_ha=final_soilN, denitrification_kgN_ha=denitrification, NO3_leached_kgN_ha=NO3_leaching, fert_app_kgN_ha=fert_app, N_min_kgN_ha=N_min, N_imm_kgN_ha=N_imm, precip_cm=precip, irrigation_cm=irr, runoff_cm=runoff, evap_cm=evap, trans_cm=trans, DP_cm=DP)
+  print(result)
+}
+overall_results(projectName = 'BaseRuns', soil = 'CoarseSHR')
+overall_results(projectName = 'SteadyStateRuns', soil = 'CoarseSHR')
+
+overall_results(projectName = 'BaseRuns', soil = 'LoamySHR')
+overall_results(projectName = 'SteadyStateRuns', soil = 'LoamySHR')
+overall_results(projectName = 'FloodRuns_v1', soil = 'LoamySHR')
 #calculate daily net mineralization
 # projectName <- 'Parlier_1983_2021test'
 # soil <- 'CoarseSHR_automation'
@@ -109,13 +146,16 @@ N_MBL <- function(projectName, soil) {
   dailyImm <- df[grepl('DAILY IMMOBILIZATION', df)]
   dailyMin <- as.numeric(gsub('DAILY MINERALIZATION', '', dailyMin))
   dailyImm <- as.numeric(gsub('DAILY IMMOBILIZATION', '', dailyImm))
-  result <- data.frame(date=seq(as.Date("1983/10/1"), as.Date("2021/4/30"), "days"), mineralization_kg_ha=dailyMin, immobilization_kg_ha=dailyImm,stringsAsFactors = FALSE)
+  result <- data.frame(date=seq(as.Date("1983/10/1"), as.Date("2020/10/1"), "days"), mineralization_kg_ha=dailyMin, immobilization_kg_ha=dailyImm,stringsAsFactors = FALSE)
   result$net_min_kg_ha <- result$mineralization_kg_ha - result$immobilization_kg_ha
   result$year <- format.Date(result$date, '%Y')
   result$month <- format.Date(result$date, '%B')
   result
 }
-n_mbl <- N_MBL(projectName = 'Parlier_1983_2021test', soil = 'CoarseSHR_automation')
+n_mbl <- N_MBL(projectName = 'SteadyStateRuns', soil = 'CoarseSHR')
+sum(n_mbl$mineralization_kg_ha) #matches total mineralization from overall summary
+sum(n_mbl$immobilization_kg_ha) #matches total immobilization from overall summary
+sum(n_mbl$net_min_kg_ha) #5182.576
 head(n_mbl)
 tapply(n_mbl$mineralization_kg_ha, n_mbl$year, sum)
 tapply(n_mbl$immobilization_kg_ha, n_mbl$year, sum)
@@ -127,4 +167,36 @@ tapply(n_mbl$net_min_kg_ha, n_mbl$year, function(x) sum(x < 0))
 tapply(n_mbl$net_min_kg_ha, n_mbl$month, sum)
 table(n_mbl$month[n_mbl$net_min_kg_ha < 0])
 
-management
+#harvest data
+harvest_results <- function(projectName, soil) {
+  input <- readLines(file.path(workDir, projectName, soil, 'MANAGE.OUT'))
+  harvest_dates <- input[which(grepl('DSSAT Crop Harvest', input))+1]
+  harvest_dates <- strsplit(harvest_dates, '-----')
+  harvest_dates <- sapply(harvest_dates, function(x) x[1])
+  harvest_dates <- gsub('    ON  ', '', harvest_dates)
+  harvest_dates <- strsplit(harvest_dates, '[/]')
+  harvest_dates <- do.call(rbind, lapply(harvest_dates, function(x) {data.frame(month=x[2], day=x[1], year=x[3], stringsAsFactors = FALSE)}))
+  crop_type <- input[grepl('DSSAT Crop Harvest', input)]
+  crop_type <- strsplit(crop_type, '----')
+  crop_type <- sapply(crop_type, function(x) x[3])
+  biomass <- input[grepl('YIELD FROM ABOVE GROUND BIOMASS', input)]
+  biomass <- strsplit(biomass, ':')
+  biomass <- sapply(biomass, function(x){as.numeric(x[2])})
+  above_biomass_N <- input[grepl('TOTAL ABOVE GROUND NITROGEN', input)]
+  above_biomass_N <- strsplit(above_biomass_N, ':')
+  above_biomass_N <- sapply(above_biomass_N, function(x){as.numeric(x[2])})
+  below_biomass_N <- input[grepl('TOTAL BELOW GROUND NITROGEN', input)]
+  below_biomass_N <- strsplit(below_biomass_N, ':')
+  below_biomass_N <- sapply(below_biomass_N, function(x){as.numeric(x[2])})
+  harvest_info <- cbind(harvest_dates, crop_type, biomass_kg_ha=biomass, above_biomass_N_kg_ha=above_biomass_N, below_biomass_N_kg_ha=below_biomass_N, total_biomass_N_kg_ha=(above_biomass_N+below_biomass_N))
+  # harvest_info$date <- 
+  harvest_info$day <- as.integer(harvest_info$day)
+  harvest_info$month <- as.integer(harvest_info$month)
+  harvest_info$year <- as.integer(harvest_info$year)
+  harvest_info$date_harvest <- as.Date(paste0(harvest_info$year, '-', harvest_info$month, '-', harvest_info$day))
+  harvest_info[,1:3] <- NULL
+  harvest_info <- harvest_info[,c(ncol(harvest_info), 1:(ncol(harvest_info)-1))]
+  print(harvest_info)
+}
+harvest_df <- harvest_results(projectName = 'BaseRuns', soil = 'LoamySHR')
+harvest_results(projectName = 'SteadyStateRuns', soil = 'LoamySHR')
