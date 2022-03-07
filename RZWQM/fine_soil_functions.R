e81037d86d372ebb54ea8ed33a2b7c88dfdfa23c
smdevine <smdevine@ucdavis.edu>
2022-03-07 13:37:12 -0800

    took out hard coding of wet years definition so as to allow for more easily running functions across different meteorological station locations.  also took out function executions from this script

--- RZWQM/fine_soil_functions.R
+++ RZWQM/fine_soil_functions.R
@@ -4,7 +4,7 @@ ssurgoDir2 <- 'C:/Users/smdevine/Desktop/post doc/Dahlke/RZWQM/ssurgo_profile_da
 list.files(ssurgoDir2)
 comp_ksat <- read.csv(file.path(ssurgoDir2, 'comp_data_RZWQMruns_Feb22.csv'), stringsAsFactors = FALSE)
 comp_ksat$AgMARplan <- paste('days', comp_ksat$irrdays, sep = '')
-stress_soils <- c('Capay', 'Willows')
+stress_soils <- c('Capay', 'Willows', 'Wekoda')
 fine_soils <- comp_ksat$compnames[which(comp_ksat$irrdays > 1 & comp_ksat$irrdays < 10)]
 fine_soils
 
@@ -36,27 +36,27 @@ lapply(AgMAR_21d_irrmonths, length)
 
 
 #IMPORTANT needs to be modified still to be flexible
-writeIrrPlan_AgMAR_LF_fine <- function(start_year=1984, end_year=2020, irrigationAssumptions='0  0  0.0  100.0  15.0  0.0  0', cornADtrigger1='0.6', cornDaystrigger1='3', cornDaystrigger2='60', cornADtrigger2='0.5', IrrAppPlanting=1.25, flood_month, flood_days, flood_app=15.0, wet_yrs) {
-  years <- start_year:end_year
-  plantings <- length(years)
-  IrrPlan <- do.call(c, lapply(years, function(x) {
-    corn_line1 <- paste('1  2  3  3 20 4', x, '1 9', x, '0 0.0  0.0', sep=' ') #date is 4/14; that #2 near the beginning should be a 3 for 'furrow'
-    corn_line2 <- '2' #this is irrigation rule #2
-    corn_line3 <- paste(' ', cornDaystrigger1, cornADtrigger1, '  ', cornDaystrigger2, cornADtrigger2, sep = ' ') 
-    corn_line4 <- '2' #this is irrigation rule #2
-    corn_line5 <- paste('', cornDaystrigger1, ' 1.0', ' ', cornDaystrigger2, ' 1.0', sep = ' ')
-    c(corn_line1, corn_line2, corn_line3, corn_line4, corn_line5)
-  }))
-  IrrPlan2 <- do.call(c, lapply(years, function(x) {
-    corn_line1 <- paste('1  1  2  1 16 4', x, '16 4', x, '0 0.0  2.0', sep=' ') #date is 4/16
-    corn_line2 <- '1' #not sure what this refers to
-    corn_line3 <- paste('16  4', x, sep = '  ')
-    if(x%in%wet_yrs) { #left off here to fix this on 12/21/21
-      c(paste('3  2  2  2', flood_days[1], flood_month[1], x, flood_days[1], flood_month[1], x, '0 0.0  2.0', sep=' '), '1', paste(flood_days[1], flood_month[1], x, sep = '  '), '1', as.character(flood_app), paste('3  2  2  2', flood_days[2], flood_month[2], x, flood_days[2], flood_month[2], x, '0 0.0  2.0', sep=' '), '1', paste(flood_days[2], flood_month[2], x, sep = '  '), '1', as.character(flood_app), paste('3  2  2  2', flood_days[3], flood_month[3], x, flood_days[3], flood_month[3], x, '0 0.0  2.0', sep=' '), '1', paste(flood_days[3], flood_month[3], x, sep = '  '), '1', as.character(flood_app), paste('3  2  2  2', flood_days[4], flood_month[4], x, flood_days[4], flood_month[4], x, '0 0.0  2.0', sep=' '), '1', paste(flood_days[4], flood_month[4], x, sep = '  '), '1', as.character(flood_app), corn_line1, corn_line2, corn_line3, as.character(IrrAppPlanting)) # the '4' denotes 4 irrigation dates
-    } else {c(corn_line1, corn_line2, corn_line3, as.character(IrrAppPlanting))} 
-  }))
-  c(paste((length(years)*2+length(wet_yrs)*4), irrigationAssumptions, sep = '  '), IrrPlan, IrrPlan2)
-}
+# writeIrrPlan_AgMAR_LF_fine <- function(start_year=1984, end_year=2020, irrigationAssumptions='0  0  0.0  100.0  15.0  0.0  0', cornADtrigger1='0.6', cornDaystrigger1='3', cornDaystrigger2='60', cornADtrigger2='0.5', IrrAppPlanting=1.25, flood_month, flood_days, flood_app=15.0, wet_yrs) {
+#   years <- start_year:end_year
+#   plantings <- length(years)
+#   IrrPlan <- do.call(c, lapply(years, function(x) {
+#     corn_line1 <- paste('1  2  3  3 20 4', x, '1 9', x, '0 0.0  0.0', sep=' ') #date is 4/14; that #2 near the beginning should be a 3 for 'furrow'
+#     corn_line2 <- '2' #this is irrigation rule #2
+#     corn_line3 <- paste(' ', cornDaystrigger1, cornADtrigger1, '  ', cornDaystrigger2, cornADtrigger2, sep = ' ') 
+#     corn_line4 <- '2' #this is irrigation rule #2
+#     corn_line5 <- paste('', cornDaystrigger1, ' 1.0', ' ', cornDaystrigger2, ' 1.0', sep = ' ')
+#     c(corn_line1, corn_line2, corn_line3, corn_line4, corn_line5)
+#   }))
+#   IrrPlan2 <- do.call(c, lapply(years, function(x) {
+#     corn_line1 <- paste('1  1  2  1 16 4', x, '16 4', x, '0 0.0  2.0', sep=' ') #date is 4/16
+#     corn_line2 <- '1' #not sure what this refers to
+#     corn_line3 <- paste('16  4', x, sep = '  ')
+#     if(x%in%wet_yrs) { #left off here to fix this on 12/21/21
+#       c(paste('3  2  2  2', flood_days[1], flood_month[1], x, flood_days[1], flood_month[1], x, '0 0.0  2.0', sep=' '), '1', paste(flood_days[1], flood_month[1], x, sep = '  '), '1', as.character(flood_app), paste('3  2  2  2', flood_days[2], flood_month[2], x, flood_days[2], flood_month[2], x, '0 0.0  2.0', sep=' '), '1', paste(flood_days[2], flood_month[2], x, sep = '  '), '1', as.character(flood_app), paste('3  2  2  2', flood_days[3], flood_month[3], x, flood_days[3], flood_month[3], x, '0 0.0  2.0', sep=' '), '1', paste(flood_days[3], flood_month[3], x, sep = '  '), '1', as.character(flood_app), paste('3  2  2  2', flood_days[4], flood_month[4], x, flood_days[4], flood_month[4], x, '0 0.0  2.0', sep=' '), '1', paste(flood_days[4], flood_month[4], x, sep = '  '), '1', as.character(flood_app), corn_line1, corn_line2, corn_line3, as.character(IrrAppPlanting)) # the '4' denotes 4 irrigation dates
+#     } else {c(corn_line1, corn_line2, corn_line3, as.character(IrrAppPlanting))} 
+#   }))
+#   c(paste((length(years)*2+length(wet_yrs)*4), irrigationAssumptions, sep = '  '), IrrPlan, IrrPlan2)
+# }
 
 #more flexible function for fine soil Ag-MAR
 writeIrrPlan_AgMAR_fine <- function(start_year=1984, end_year=2020, irrigationAssumptions='0  0  0.0  100.0  15.0  0.0  0', cornADtrigger1='0.6', cornDaystrigger1='3', cornDaystrigger2='60', cornADtrigger2='0.5', IrrAppPlanting=1.25, flood_month=NA, flood_days=NA, flood_app=15, wet_yrs, soil_Ksat) {
@@ -81,15 +81,14 @@ writeIrrPlan_AgMAR_fine <- function(start_year=1984, end_year=2020, irrigationAs
       c(paste('3  2  2  2', flood_days[1], flood_month[1], x, flood_days[length(flood_days)], flood_month[length(flood_month)], x, '0 0.0  2.0', sep=' '), paste(as.character(length(flood_days))), paste(flood_days, flood_month, x, sep = '  '), paste(as.character(length(flood_days))), paste(rep(c(rep(as.character(flood_app1), (flood_length-1)), as.character(flood_app2)), 4), collapse = ' '), corn_line1, corn_line2, corn_line3, as.character(IrrAppPlanting)) # the '4' denotes 4 irrigation dates
     } else {c(corn_line1, corn_line2, corn_line3, as.character(IrrAppPlanting))} 
   }))
-  c(paste(length(years)*2, irrigationAssumptions, sep = '  '), IrrPlan, IrrPlan2)
+  c(paste((length(years)*2+length(wet_yrs)), irrigationAssumptions, sep = '  '), IrrPlan, IrrPlan2)
 }
-
 
 # suelos = fine_soils
 # AgMAR_month = AgMAR_3d_irrmonths_early
 # AgMAR_days = AgMAR_3d_irrdays
 # comp_df = comp_ksat
-AgMAR_runs_fine <- function(stn, scn, suelos, AgMAR_month, AgMAR_days, fake_planting, fake_harvest, comp_df) {
+AgMAR_runs_fine <- function(stn, scn, suelos, AgMAR_month, AgMAR_days, fake_planting, fake_harvest, comp_df, wetyears) {
   met <- paste0(stn, '_1983_2021.MET')
   brk <- paste0(stn, '_1983_2021.BRK')
   for(i in 1:length(suelos)) {
@@ -102,32 +101,15 @@ AgMAR_runs_fine <- function(stn, scn, suelos, AgMAR_month, AgMAR_days, fake_plan
     copyRZINIT(stn, scn, suelos[i])
     gen_input <- templateRZWQM(stn, scn, suelos[i], 'RZWQM_init.DAT')
     suelo_input <- soilInfo(stn, scn, suelos[i], 'RZWQM.DAT')
-    planting <- writePlantingPlan_AgMAR(wet_years=as.integer(c("1995", "2010", "1992", "1993", "1987", "1998", "2006", "1991", "1986", "1994")), rye_date_planting = fake_planting, rye_date_harvest = fake_harvest)
+    planting <- writePlantingPlan_AgMAR(wet_years=as.integer(wetyears), rye_date_planting = fake_planting, rye_date_harvest = fake_harvest)
     fert <- writeFertPlan(end_year = 2020)
     tillage <- writeTillagePlan(depth = '15.0')
-    Irr <- if(suelos[i] %in% stress_soils) {writeIrrPlan_AgMAR_fine(cornADtrigger1 = '0.8', cornADtrigger2 = '0.7', flood_month=AgMAR_m, flood_days=AgMAR_d, wet_yrs=as.integer(c("1995", "2010", "1992", "1993", "1987", "1998", "2006", "1991", "1986", "1994")), soil_Ksat = soilksat)} else{writeIrrPlan_AgMAR_fine(flood_month=AgMAR_m, flood_days=AgMAR_d, wet_yrs=as.integer(c("1995", "2010", "1992", "1993", "1987", "1998", "2006", "1991", "1986", "1994")), soil_Ksat = soilksat)}
+    Irr <- if(suelos[i] %in% stress_soils) {writeIrrPlan_AgMAR_fine(cornADtrigger1 = '0.8', cornADtrigger2 = '0.7', flood_month=AgMAR_m, flood_days=AgMAR_d, wet_yrs=as.integer(wetyears), soil_Ksat = soilksat)} else{writeIrrPlan_AgMAR_fine(flood_month=AgMAR_m, flood_days=AgMAR_d, wet_yrs=as.integer(wetyears), soil_Ksat = soilksat)}
     writeRZWQM(station = stn, scenario = scn, soil = suelos[i], input=gen_input, soil_input = suelo_input, planting_data = planting, fert_plan = fert, tillage_plan = tillage, Irr_plan = Irr)
     steady_state_setup(station = stn, scenario = scn, soil = suelos[i], type = 'run 2', soil_input = suelo_input)
   }
 }
 
-#run functions for list of fine soils with irrdays > 1 and < 10
-#Jan 3-day interval
-AgMAR_runs_fine(stn = 'Parlier', scn = 'AgMAR_Jan3d', suelos = fine_soils, AgMAR_month = AgMAR_3d_irrmonths_early, AgMAR_days = AgMAR_3d_irrdays_early,  fake_planting = '1    1   1992', fake_harvest = '25 2 1992', comp_df = comp_ksat)
-writeOverallResults(compnames = fine_soils, scenario = 'AgMAR_Jan3d', weather_stn = 'Parlier')
 
-#Jan 7-day interval
-AgMAR_runs_fine(stn = 'Parlier', scn = 'AgMAR_Jan7d', suelos = fine_soils, AgMAR_month = AgMAR_7d_irrmonths_early, AgMAR_days = AgMAR_7d_irrdays_early,  fake_planting = '1    1   1992', fake_harvest = '25 2 1992', comp_df = comp_ksat)
-writeOverallResults(compnames = fine_soils, scenario = 'AgMAR_Jan7d', weather_stn = 'Parlier')
-
-#Mar 3-day interval
-AgMAR_runs_fine(stn = 'Parlier', scn = 'AgMAR_Mar3d', suelos = fine_soils, AgMAR_month = AgMAR_3d_irrmonths_late, AgMAR_days = AgMAR_3d_irrdays_late,  fake_planting = '9    2   1992', fake_harvest = '13 4 1992', comp_df = comp_ksat)
-writeOverallResults(compnames = fine_soils, scenario = 'AgMAR_Mar3d', weather_stn = 'Parlier')
-
-#Mar 7-day interval
-AgMAR_runs_fine(stn = 'Parlier', scn = 'AgMAR_Mar7d', suelos = fine_soils, AgMAR_month = AgMAR_7d_irrmonths_late, AgMAR_days = AgMAR_7d_irrdays_late,  fake_planting = '9    2   1992', fake_harvest = '13 4 1992', comp_df = comp_ksat)
-writeOverallResults(compnames = fine_soils, scenario = 'AgMAR_Mar7d', weather_stn = 'Parlier')
-
-#21-day interval
-AgMAR_runs_fine(stn = 'Parlier', scn = 'AgMAR_21d', suelos = fine_soils, AgMAR_month = AgMAR_21d_irrmonths, AgMAR_days = AgMAR_21d_irrdays,  fake_planting = '1    1   1992', fake_harvest = '13 4 1992', comp_df = comp_ksat)
-writeOverallResults(compnames = fine_soils, scenario = 'AgMAR_21d', weather_stn = 'Parlier')
+comp_ksat[comp_ksat$compnames=='Wekoda',]
+comp_ksat[comp_ksat$compnames=='Lofgren',]
