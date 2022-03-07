0e534b545590d1f61cd4d180feb3259b81057b3e
smdevine <smdevine@ucdavis.edu>
2022-03-07 13:33:02 -0800

    minor changes to execute functions

--- RZWQM/cleanRZWQMdirs.R
+++ RZWQM/cleanRZWQMdirs.R
@@ -1,6 +1,6 @@
-workDir <- 'C:/Users/smdevine/Desktop/post doc/Dahlke/RZWQM/projects/PulseSoilClimate/ClimateRuns'
-compnames <- c('Hanford', 'Delhi', 'Wasco', 'Hesperia', 'Milham', 'Tujunga', 'Panoche', 'Cerini', 'Yolo', 'Colpien', 'Tehama', 'Capay', 'Clear Lake', 'Willows', 'Tulare', 'CoarseSHR', 'LoamySHR', 'FineSHR')
-compnames2 <- c('Kimberlina', 'Columbia', 'Excelsior', 'Atwater', 'Rincon', 'Sycamore', 'Conejo', 'Westhaven', 'Pleito', 'Porterville', 'Lokern', 'Merced', 'Cropley', 'Tachi', 'Myers')
+station <- 'Parlier'
+workDir <- 'C:/Users/smdevine/Desktop/post doc/Dahlke/RZWQM/projects/PulseSoilClimate/ClimateRuns_Silage'
+compnames <- c('Hanford', 'Delhi', 'Wasco', 'Hesperia', 'Milham', 'Tujunga', 'Panoche', 'Cerini', 'Yolo', 'Colpien', 'Tehama', 'Capay', 'Clear Lake', 'Tulare', 'Kimberlina', 'Columbia', 'Excelsior', 'Atwater', 'Rincon', 'Sycamore', 'Conejo', 'Westhaven', 'Pleito', 'Lokern', 'Merced', 'Cropley', 'Tachi', 'Myers', 'Lofgren', 'Wekoda')
 cleanRZWQMdirs <- function(stn, scn, soilnames, fnames=c('LAYER.PLT', 'MBLCARBON.OUT', 'RESIDUES.OUT', 'MBLWAT.OUT')) {
   for(j in seq_along(soilnames)) {
     for(i in seq_along(fnames)) {
@@ -8,16 +8,9 @@ cleanRZWQMdirs <- function(stn, scn, soilnames, fnames=c('LAYER.PLT', 'MBLCARBON
     }
   }
 }
-cleanRZWQMdirs(stn = 'Parlier', scn = 'SteadyStateRuns', soilnames = compnames)
-cleanRZWQMdirs(stn = 'Parlier', scn = 'AgMAR_Jan3d', soilnames = compnames)
-cleanRZWQMdirs(stn = 'Parlier', scn = 'AgMAR_Jan7d', soilnames = compnames)
-cleanRZWQMdirs(stn = 'Parlier', scn = 'AgMAR_Mar3d', soilnames = compnames)
-cleanRZWQMdirs(stn = 'Parlier', scn = 'AgMAR_Mar7d', soilnames = compnames)
-cleanRZWQMdirs(stn = 'Parlier', scn = 'AgMAR_21d', soilnames = compnames)
-
-cleanRZWQMdirs(stn = 'Parlier', scn = 'SteadyStateRuns', soilnames = compnames2)
-cleanRZWQMdirs(stn = 'Parlier', scn = 'AgMAR_Jan3d', soilnames = compnames2)
-cleanRZWQMdirs(stn = 'Parlier', scn = 'AgMAR_Jan7d', soilnames = compnames2)
-cleanRZWQMdirs(stn = 'Parlier', scn = 'AgMAR_Mar3d', soilnames = compnames2)
-cleanRZWQMdirs(stn = 'Parlier', scn = 'AgMAR_Mar7d', soilnames = compnames2)
-cleanRZWQMdirs(stn = 'Parlier', scn = 'AgMAR_21d', soilnames = compnames2)
+cleanRZWQMdirs(stn = station, scn = 'SteadyStateRuns', soilnames = compnames)
+cleanRZWQMdirs(stn = station, scn = 'AgMAR_Jan3d', soilnames = compnames)
+cleanRZWQMdirs(stn = station, scn = 'AgMAR_Jan7d', soilnames = compnames)
+cleanRZWQMdirs(stn = station, scn = 'AgMAR_Mar3d', soilnames = compnames)
+cleanRZWQMdirs(stn = station, scn = 'AgMAR_Mar7d', soilnames = compnames)
+cleanRZWQMdirs(stn = station, scn = 'AgMAR_21d', soilnames = compnames)
