d76a28fc3a1e8289bd166bd4af13bac63241ee31
smdevine <smdevine@ucdavis.edu>
2022-04-28 16:57:13 -0700

    added function to calculate profile weighted means of several soil props

--- RZWQM/comp_ksat_calc.R
+++ RZWQM/comp_ksat_calc.R
@@ -1,3 +1,6 @@
+#TO-DO
+#calc profile weighted texture, wilting point, and field capacity
+#read-in microporosity estimates from rzqwm created input files
 library(soilDB)
 library(aqp)
 ssurgoDir <- 'C:/Users/smdevine/Desktop/post doc/Dahlke/RZWQM/ssurgo_profile_data/RZWQM input'
@@ -12,6 +15,11 @@ ssurgo_horizons <- rbind(ssurgo_horizons, ssurgo_horizons2)
 ssurgo_horizons$texture <- textural.class.calc(sand = ssurgo_horizons$sandtotal_r, silt = ssurgo_horizons$silttotal_r, clay = ssurgo_horizons$claytotal_r)
 comp_data <- data.frame(compnames=unique(ssurgo_horizons$compname), stringsAsFactors = FALSE)
 comp_data$SHR <- ssurgo_horizons$SHRname[match(comp_data$compnames, ssurgo_horizons$compname)]
+ssurgo_horizons[ssurgo_horizons$compname=='Colpien',]
+ssurgo_horizons[ssurgo_horizons$compname=='Tehama',]
+ssurgo_horizons[ssurgo_horizons$compname=='Pleito',]
+ssurgo_horizons[ssurgo_horizons$compname=='Columbia',]
+ssurgo_horizons[ssurgo_horizons$compname=='Westhaven',]
 
 #create soil profile collection object
 #initialize spc object
@@ -27,10 +35,46 @@ profile_wtd_ksat <- profileApply(SSURGO_spc, FUN=function(x) {
   horizon_wts <- (x$hzdepb_r - x$hzdept_r) / profile_depth
   sum(x$ksat_r * horizon_wts)*24*3600/10000
 })
-
+profile_wtd_clay <- profileApply(SSURGO_spc, FUN=function(x) {
+  profile_depth <- max(x$hzdepb_r)
+  horizon_wts <- (x$hzdepb_r - x$hzdept_r) / profile_depth
+  sum(x$claytotal_r * horizon_wts)
+})
+profile_wtd_silt <- profileApply(SSURGO_spc, FUN=function(x) {
+  profile_depth <- max(x$hzdepb_r)
+  horizon_wts <- (x$hzdepb_r - x$hzdept_r) / profile_depth
+  sum(x$silttotal_r * horizon_wts)
+})
+profile_wtd_sand <- profileApply(SSURGO_spc, FUN=function(x) {
+  profile_depth <- max(x$hzdepb_r)
+  horizon_wts <- (x$hzdepb_r - x$hzdept_r) / profile_depth
+  sum(x$sandtotal_r * horizon_wts)
+})
+profile_wtd_WP <- profileApply(SSURGO_spc, FUN=function(x) {
+  profile_depth <- max(x$hzdepb_r)
+  horizon_wts <- (x$hzdepb_r - x$hzdept_r) / profile_depth
+  sum(x$wfifteenbar_r * horizon_wts)
+})
+profile_wtd_FC <- profileApply(SSURGO_spc, FUN=function(x) {
+  profile_depth <- max(x$hzdepb_r)
+  horizon_wts <- (x$hzdepb_r - x$hzdept_r) / profile_depth
+  sum(x$wthirdbar_r * horizon_wts)
+})
+profile_wtd_microporosity <- profileApply(SSURGO_spc, FUN=function(x) {
+  profile_depth <- max(x$hzdepb_r)
+  horizon_wts <- (x$hzdepb_r - x$hzdept_r) / profile_depth
+  sum(x$Microporosity * horizon_wts)
+})
 comp_data$cokey <- ssurgo_horizons$cokey[match(comp_data$compnames, ssurgo_horizons$compname)]
 comp_data$ksat_cm_day <- profile_wtd_ksat[match(comp_data$cokey, as.integer(names(profile_wtd_ksat)))]
-
 comp_data$irrdays <- ifelse(comp_data$ksat_cm_day >= 15, 1, ceiling(15 / comp_data$ksat_cm_day))
+comp_data$clay <- profile_wtd_clay[match(comp_data$cokey, as.integer(names(profile_wtd_clay)))]
+comp_data$silt <- profile_wtd_silt[match(comp_data$cokey, as.integer(names(profile_wtd_silt)))]
+comp_data$sand <- profile_wtd_sand[match(comp_data$cokey, as.integer(names(profile_wtd_sand)))]
+comp_data$theta_15b <- profile_wtd_WP[match(comp_data$cokey, as.integer(names(profile_wtd_WP)))]
+comp_data$theta_0.3b <- profile_wtd_FC[match(comp_data$cokey, as.integer(names(profile_wtd_FC)))]
+comp_data$microporosity <- profile_wtd_microporosity[match(comp_data$cokey, as.integer(names(profile_wtd_microporosity)))]
 comp_data[order(comp_data$SHR),]
+
 write.csv(comp_data, file.path(ssurgoDir, 'comp_data_RZWQMruns_Feb22.csv'), row.names = FALSE)
+utils::View(comp_data[order(comp_data$SHR),])
