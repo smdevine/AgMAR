d6ff8f5f4c6348ab93f03221bf773e757dc3289c
smdevine <smdevine@ucdavis.edu>
2023-04-02 17:19:13 -0700

    minor changes

--- RZWQM/writeResults.R
+++ RZWQM/writeResults.R
@@ -1,6 +1,6 @@
 workDir <- 'C:/Users/smdevine/Desktop/post doc/Dahlke/RZWQM/projects/PulseSoilClimate/ClimateRuns_Silage/'
-met_stn <- 'Shafter'
-compnames <- c('Hanford', 'Delhi', 'Wasco', 'Hesperia', 'Milham', 'Tujunga', 'Panoche', 'Cerini', 'Yolo', 'Colpien', 'Tehama', 'Capay', 'Clear Lake', 'Tulare', 'Kimberlina', 'Columbia', 'Excelsior', 'Atwater', 'Rincon', 'Sycamore', 'Conejo', 'Westhaven', 'Pleito', 'Lokern', 'Merced', 'Cropley', 'Tachi', 'Myers', 'Lofgren', 'Wekoda')
+# met_stn <- 'Shafter'
+# compnames <- c('Hanford', 'Delhi', 'Wasco', 'Hesperia', 'Milham', 'Tujunga', 'Panoche', 'Cerini', 'Yolo', 'Colpien', 'Tehama', 'Capay', 'Clear Lake', 'Tulare', 'Kimberlina', 'Columbia', 'Excelsior', 'Atwater', 'Rincon', 'Sycamore', 'Conejo', 'Westhaven', 'Pleito', 'Lokern', 'Merced', 'Cropley', 'Tachi', 'Myers', 'Lofgren', 'Wekoda')
 #Wekoda, Clear Lake, and Tulare also defined as stress soils on 2/28/22 and Capay v2 on 3/1/22
 writeOverallResults(compnames = compnames, scenario = 'SteadyStateRuns', weather_stn = met_stn)
 writeOverallResults(compnames = compnames, scenario = 'AgMAR_Jan3d', weather_stn = met_stn)
